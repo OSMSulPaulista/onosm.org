f35330abcc2284871432974475ce929ef3fc37d6
Alexandre Magno <alexandre.mbm@gmail.com>
2014-04-29 20:55:39 -0300

    Improved gencat.sh. Closes #11

--- gencat.sh
+++ gencat.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-
-cp categories.json categories.json-backup
-sort -u categories.json-backup -o categories.json
-sed -i 2d categories.json
-echo ']' >> categories.json
-
+#
 # JSON validator like jsonlint.com
 
-cat categories.json | python -m json.tool 1>/dev/null
-jsonlint -v categories.json
+function regenerate() {
+
+    JSON_FILE="$1"
+
+    cp "$JSON_FILE" "$JSON_FILE-backup"
+    sort -u "$JSON_FILE-backup" -o "$JSON_FILE"
+    sed -i 2d "$JSON_FILE"
+    echo ']' >> "$JSON_FILE"
+
+}
+
+function validate() {
+
+    JSON_FILE="$1"
+
+    cat "$JSON_FILE" | python -m json.tool 1>/dev/null
+    jsonlint -v "$JSON_FILE"
+
+}
+
+function makeTo() {
+
+    JSON_FILE="$1"
+
+    regenerate "$JSON_FILE"
+    validate "$JSON_FILE"
+
+}
+
+makeTo "categories.json"
+makeTo "payment.json"
